78e59eefb389ddb554b2ed2822328e29433e1c4f
unknown <renataub2964@gmail.com>
2024-10-07 17:34:31 +0300

    navbar

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './smartLight/About';
+import AllProducts from './smartLight/AllProducts';
+import Contact from './smartLight/Contact';
+import Home from './smartLight/Home'; 
+import Navbar from './smartLight/Navbar';
+import Product from './smartLight/Product';
+import Projects from './smartLight/Projects';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route exact path="/" element={<Home></Home>} />
+          <Route exact path="/About" element={<About></About>} />
+          <Route exact path="/AllProducts" element={<AllProducts></AllProducts>} />
+          <Route exact path="/Contact" element={<Contact></Contact>} />
+          <Route exact path="/Product" element={<Product></Product>} />
+          <Route exact path="/Projects" element={<Projects></Projects>} />
+        </Routes>
+      </BrowserRouter>
+        
+
+    </>
   );
 }
 
